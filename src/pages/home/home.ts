dd5ad20c087a70c22eb0a7e4a0ff2500d61a75d3
Paulo André Soares de Mendonça <pauloandresoares@gmail.com>
2017-11-25 16:27:10 -0200

    [FEAT] Código da aplicação

---
+++ src/pages/home/home.ts
@@ -0,0 +1,103 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { GoogleMap, GoogleMapsEvent } from '@ionic-native/google-maps';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  map: GoogleMap;
+  devicePosition: Geoposition;
+  messageError: string = '';
+
+  constructor(public navCtrl: NavController, public platform: Platform, private geolocation: Geolocation) {
+    platform.ready().then(() => {
+      //this.loadMap();
+      let watch = this.geolocation.watchPosition();
+      watch.subscribe((data) => {
+
+         this.map = new GoogleMap('map', {
+   
+           'controls': {
+             'compass': true,
+             'myLocationButton': true,
+             'indoorPicker': true,
+             'zoom': true
+           },
+           'gestures': {
+             'scroll': true,
+             'tilt': true,
+             'rotate': true,
+             'zoom': true
+           },
+           'camera': {
+              target: {
+                lat: data.coords.latitude,
+                lng: data.coords.longitude
+              },
+             'tilt': 30,
+             'zoom': 15,
+             'bearing': 50
+           }
+         });
+
+          this.map.addMarker({
+            title: 'Ionic',
+            icon: 'blue',
+            animation: 'DROP',
+            position: {
+              lat: 43.0741904,
+              lng: -89.3809802
+            }
+          })
+          .then(marker => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK)
+              .subscribe(() => {
+                alert('clicked');
+              });
+          });
+
+         this.map.setClickable(true);
+
+
+         this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+           console.log('Map is ready!');
+         });
+
+         this.map.on(GoogleMapsEvent.POLYLINE_CLICK)
+              .subscribe(() => {
+                alert('long click');
+              });
+
+
+
+
+      });
+    });
+
+    
+  }
+
+  ionViewDidLoad() {
+    this.geolocation.getCurrentPosition().then((position) => {
+      this.devicePosition = position;
+    }, (err) => {
+      this.messageError = err.message;
+    });
+}
+  
+  addMarker() {
+      this.map.clear();
+      this.map.addMarker({
+            title: 'Novo ponto local',
+            animation: 'DROP',
+            position: {
+              lat: this.devicePosition.coords.latitude,
+              lng: this.devicePosition.coords.longitude
+            }
+          });
+  }
+
+}
